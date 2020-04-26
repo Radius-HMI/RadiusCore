bf15947ee0cd7d8cda484e1b61c1f2f576014d20
David Cox <david.dmc.cox@gmail.com>
2020-04-26 10:32:18 -0500

    Initial

--- Source/RadiusCore1/RadiusCore/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
+++
@@ -1,7 +0,0 @@
-namespace RadiusCore.Areas.HelpPage.ModelDescriptions
-{
-    public class CollectionModelDescription : ModelDescription
-    {
-        public ModelDescription ElementDescription { get; set; }
-    }
-}
